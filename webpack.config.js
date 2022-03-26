abba31166a6f3bc95e01c37eaddab9554116d5a6
yeongno <dldudsh83@gmail.com>
2022-03-27 00:34:46 +0900

    plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 //import
 const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
 
 //export
 module.exports={//parcel index.html
@@ -11,5 +12,14 @@ module.exports={//parcel index.html
     // path: path.resolve(__dirname, 'dist'),
     // filename:'mains.js',
     clean: true
-  }
+  },
+
+  //번들링 후 결과물의 처리 방식 등 다양한 플러그인들을 설정
+  plugins: [
+    new HtmlPlugin({
+      template: './index.html'
+    })
+  ],
+
+
 }
