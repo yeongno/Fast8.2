641fc80b75872f63d1e80f4915ff45c09f5af95f
yeongno <dldudsh83@gmail.com>
2022-03-27 00:59:05 +0900

    정적 파일 연결

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 //import
 const path = require('path')
 const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 //export
 module.exports={//parcel index.html
@@ -18,6 +19,11 @@ module.exports={//parcel index.html
   plugins: [
     new HtmlPlugin({
       template: './index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {from:'static'}
+      ]
     })
   ],
 
